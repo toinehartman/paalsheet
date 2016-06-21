45240b00eda845d7f141c62b3175288b9b31a044
Verzijl <marcmverzijl@gmail.com>
2016-06-21 22:38:08 +0200

    Timetable.js toegevoegd. CSS aanpassingen voor schaling.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,6 +12,23 @@
     $onInit() {
       this.$http.get('/api/bondsleden')
         .then(response => this.bondsleden = response.data);
+
+      var timetable = new Timetable();
+
+      timetable.setScope(7,3)
+
+      timetable.addLocations(['Maandag', 'Dinsdag', 'Woensdag', 'Donderdag']);
+
+      timetable.addEvent('Sightseeing', 'Maandag', new Date(2016,7,17,10,45), new Date(2016,7,17,12,30), '#');
+      timetable.addEvent('Zumba', 'Dinsdag', new Date(2016,7,17,12), new Date(2016,7,17,13), '#');
+      timetable.addEvent('Zumbu', 'Dinsdag', new Date(2016,7,17,13,30), new Date(2016,7,17,15), '#');
+      timetable.addEvent('Lasergaming', 'Donderdag', new Date(2016,7,17,17,45), new Date(2016,7,17,19,30), '#');
+      timetable.addEvent('Tokyo Hackathon Livestream', 'Woensdag', new Date(2016,7,17,12,30), new Date(2016,7,17,16,15)); // url is optional and is not used for this event
+
+      var renderer = new Timetable.Renderer(timetable);
+      renderer.draw('.timetable');
+    
+
     }
   }
 
