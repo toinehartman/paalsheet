407924a5473ac466bef144c477f4b1fffa8e14d1
Toine Hartman <tjbhartman@gmail.com>
2016-07-05 15:51:55 +0200

    Added list of tasks to bondslid.

--- server/api/bondslid/bondslid.model.js
+++ server/api/bondslid/bondslid.model.js
@@ -16,7 +16,11 @@ var BondslidSchema = new mongoose.Schema({
   email: {
     type: String,
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-  }
+  },
+  tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Taak'
+  }]
 });
 
 export default mongoose.model('Bondslid', BondslidSchema);
