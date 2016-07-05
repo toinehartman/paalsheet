10b4db7892c24e0d2e90be6cdd854c634298cad5
Verzijl <marcmverzijl@gmail.com>
2016-07-05 11:42:26 +0200

    Little cleanup.

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function(config) {
       // bower:js
       'client/bower_components/jquery/dist/jquery.js',
       'client/bower_components/angular/angular.js',
+      'client/bower_components/bootstrap/dist/js/bootstrap.js',
       'client/bower_components/angular-resource/angular-resource.js',
       'client/bower_components/angular-cookies/angular-cookies.js',
       'client/bower_components/angular-sanitize/angular-sanitize.js',
