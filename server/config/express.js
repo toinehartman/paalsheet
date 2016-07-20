248e87fab373e8fd1af9ba3d2924f228cc108f1d
Toine Hartman <tjbhartman@gmail.com>
2016-07-20 17:49:43 +0200

    Better logs.

--- server/config/express.js
+++ server/config/express.js
@@ -33,7 +33,7 @@ export default function(app) {
 
   app.set('appPath', path.join(config.root, 'client'));
   app.use(express.static(app.get('appPath')));
-  app.use(morgan('dev'));
+  app.use(morgan('short'));
 
   app.set('views', config.root + '/server/views');
   app.set('view engine', 'jade');
